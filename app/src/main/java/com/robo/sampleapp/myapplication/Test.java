32c50ffb9786aa3ec8fbab947cffcfa0dd04b692
manojmandal007 <manoj.mandal@robosoftin.com>
2016-08-11 09:46:37 +0530

    new class

---
+++ app/src/main/java/com/robo/sampleapp/myapplication/Test.java
@@ -0,0 +1,15 @@
+package com.robo.sampleapp.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by manoj on 11/8/16.
+ */
+public class Test extends Fragment {
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
